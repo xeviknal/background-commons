b40b5361f27f62cf6869111ec4e851a9d3786e2d
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-08-28 01:20:50 +0200

    Add cancelled_at to jobs

--- models/job.go
+++ models/job.go
@@ -5,14 +5,15 @@ import (
 )
 
 type Job struct {
-	Id         int64          `db:"id" json:"id"`
-	ObjectId   int64          `db:"object_id" json:"object_id"`
-	Sleep      *time.Duration `db:"sleep" json:"sleep"`
-	Status     *string        `db:"status" json:"status"`
-	CreatedAt  time.Time      `db:"created_at" json:"created_at"`
-	QueuedAt   *time.Time     `db:"queued_at" json:"queued_at"`
-	StartedAt  *time.Time     `db:"started_at" json:"started_at"`
-	FinishedAt *time.Time     `db:"finished_at" json:"finished_at"`
+	Id          int64          `db:"id" json:"id"`
+	ObjectId    int64          `db:"object_id" json:"object_id"`
+	Sleep       *time.Duration `db:"sleep" json:"sleep"`
+	Status      *string        `db:"status" json:"status"`
+	CreatedAt   time.Time      `db:"created_at" json:"created_at"`
+	QueuedAt    *time.Time     `db:"queued_at" json:"queued_at"`
+	StartedAt   *time.Time     `db:"started_at" json:"started_at"`
+	FinishedAt  *time.Time     `db:"finished_at" json:"finished_at"`
+	CancelledAt *time.Time     `db:"cancelled_at" json:"cancelled_at"`
 }
 
 func NewJob(ObjectId int64) Job {
