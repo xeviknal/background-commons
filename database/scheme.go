345e5e27ce0fe8c137f70aafc83fbe7778807073
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-08-27 17:07:22 +0200

    Allowing define notnull property to model fields (#3)
    * Allowing define notnull property to model fields
    
    * No need to use tags for PK

--- database/scheme.go
+++ database/scheme.go
@@ -1,17 +1,29 @@
 package database
 
-import "github.com/xeviknal/background-commons/models"
+import (
+	"github.com/xeviknal/background-commons/models"
+)
 
 type tables []table
 type table struct {
-	Name  string
-	Model interface{}
-	Key   string
+	Name    string
+	Model   interface{}
+	Key     string
+	Columns columns
+}
+
+type columns []column
+type column struct {
+	Name    string
+	NotNull bool
 }
 
 func CreateScheme() error {
 	for _, table := range getTables() {
-		dbmap.AddTableWithName(table.Model, table.Name).SetKeys(true, table.Key)
+		t := dbmap.AddTableWithName(table.Model, table.Name).SetKeys(true, table.Key)
+		for _, column := range table.Columns {
+			t.ColMap(column.Name).SetNotNull(column.NotNull)
+		}
 	}
 	// TODO: Implement migration tool (rubenv/sql-migrate)
 	return dbmap.CreateTablesIfNotExists()
@@ -27,6 +39,14 @@ func getTables() tables {
 			"jobs",
 			models.Job{},
 			"Id",
+			columns{
+				{"Id", true},
+				{"ObjectId", true},
+				{"CreatedAt", true},
+				{"Sleep", false},
+				{"FinishedAt", false},
+				{"StartedAt", false},
+			},
 		},
 	}
 }
