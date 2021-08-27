73b2b62e7008fdad90840b6c91ec91aaef7dc285
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-08-27 09:45:15 +0200

    Initial commit

---
+++ database/scheme.go
@@ -0,0 +1,32 @@
+package database
+
+import "github.com/xeviknal/background-commons/models"
+
+type tables []table
+type table struct {
+	Name  string
+	Model interface{}
+	Key   string
+}
+
+func CreateScheme() error {
+	for _, table := range getTables() {
+		dbmap.AddTableWithName(table.Model, table.Name).SetKeys(true, table.Key)
+	}
+	// TODO: Implement migration tool (rubenv/sql-migrate)
+	return dbmap.CreateTablesIfNotExists()
+}
+
+func DropTables() error {
+	return dbmap.DropTablesIfExists()
+}
+
+func getTables() tables {
+	return tables{
+		{
+			"jobs",
+			models.Job{},
+			"Id",
+		},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/xeviknal/background-commons
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/lib/pq v1.10.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.8 // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
+	gopkg.in/gorp.v1 v1.7.2
+)
